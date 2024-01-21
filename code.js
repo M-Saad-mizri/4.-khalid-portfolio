ed5bea25625e30acd3a98453e6e3ba553b457bd0
Muhammad Saad <saadmizri434@gmail.com>
2024-01-21 12:09:24 +0500

    snapchat added

--- code.js
+++ code.js
@@ -15,5 +15,8 @@ document.getElementById('thumbnail').addEventListener('click', function() {
   document.getElementById('overlayyyy').addEventListener('click', function(event) {
     if (event.target.id === 'overlayyyy') {
       document.getElementById('overlayyyy').style.display = 'none';
+      document.getElementById('thumbnail').style.display = 'block';
+      document.getElementById('name').style.display = 'block';
+      document.getElementById('social-media').style.display = 'flex';
     }
   });
